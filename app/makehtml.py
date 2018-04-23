6ff2aebe1554d2bd1bc7083fd899b9fdbeaa9329
amunch <amunch@nd.edu>
2018-04-23 01:15:58 -0400

    Marky mark and the Funky Bunch

--- app/makehtml.py
+++ app/makehtml.py
@@ -2,17 +2,133 @@ import csv
 
 if __name__ == "__main__":
     filename = "hm.html"
-    with open(filename,"w+") as f:
-        firstpart = "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Heatmaps</title><style>#map {height: 100%;}html, body {height: 100%;margin: 0;padding: 0;}#floating-panel {position: absolute;top: 10px;left: 25%;z-index: 5;background-color: #fff;padding: 5px;border: 1px solid #999;text-align: center;font-family: \'Roboto\',\'sans-serif\';line-height: 30px;padding-left: 10px;}#floating-panel {background-color: #fff;border: 1px solid #999;left: 25%;padding: 5px;position: absolute;top: 10px;z-index: 5;}</style></head><body><div id=\"floating-panel\"><button onclick=\"toggleHeatmap()\">Toggle Heatmap</button><button onclick=\"changeGradient()\">Change gradient</button><button onclick=\"changeRadius()\">Change radius</button><button onclick=\"changeOpacity()\">Change opacity</button></div><div id=\"map\"></div><script>var map,heatmap;function initMap() {map = new google.maps.Map(document.getElementById('map'), {zoom: 13,center: {lat: 41.8, lng: -87.6},mapTypeId: 'satellite'});heatmap = new google.maps.visualization.HeatmapLayer({data: getPoints(),map: map});}function toggleHeatmap() {heatmap.setMap(heatmap.getMap() ? null : map);}function changeGradient() {var gradient = ['rgba(0, 255, 255, 0)','rgba(0, 255, 255, 1)','rgba(0, 191, 255, 1)','rgba(0, 127, 255, 1)','rgba(0, 63, 255, 1)','rgba(0, 0, 255, 1)','rgba(0, 0, 223, 1)','rgba(0, 0, 191, 1)','rgba(0, 0, 159, 1)','rgba(0, 0, 127, 1)','rgba(63, 0, 91, 1)','rgba(127, 0, 63, 1)','rgba(191, 0, 31, 1)','rgba(255, 0, 0, 1)']heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);}function changeRadius() {heatmap.set('radius', heatmap.get('radius') ? null : 20);}function changeOpacity() {heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);}function getPoints() {output = ["
-        f.write(firstpart)
-        with open("temp.txt","r+") as fi:
-            reader = csv.reader(fi)
-            first = True
-            for row in reader:
-                if first:
-                      f.write("new google.maps.LatLng("+row[0]+","+row[1]+")")
-                      first = False
-                else:
-                      f.write(",new google.maps.LatLng("+row[0]+","+row[1]+")")
-        secondpart = "];return output;}</script><script async defersrc=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBmTUg4O2hqYtjaIksOPykzdiYkrGhVgME&libraries=visualization&callback=initMap\"></script></body></html>"
-        f.write(secondpart)
+    html = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Heatmaps</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
+      #floating-panel {
+        background-color: #fff;
+        border: 1px solid #999;
+        left: 25%;
+        padding: 5px;
+        position: absolute;
+        top: 10px;
+        z-index: 5;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="floating-panel">
+      <button onclick="toggleHeatmap()">Toggle Heatmap</button>
+      <button onclick="changeGradient()">Change gradient</button>
+      <button onclick="changeRadius()">Change radius</button>
+      <button onclick="changeOpacity()">Change opacity</button>
+    </div>
+    <div id="map"></div>
+<script>
+
+      // This example requires the Visualization library. Include the libraries=visualization
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCprBsoVtLAhqFBrOSPfvMczXFUwl0DrAM&libraries=visualization">
+
+    
+
+      var map, heatmap;
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: {lat: 41.8781, lng: -87.6298},
+          mapTypeId: 'satellite'
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+          data: getPoints(),
+          map: map
+        });
+      }
+
+      function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+      }
+
+      function changeGradient() {
+        var gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+      }
+
+      function changeRadius() {
+        heatmap.set('radius', heatmap.get('radius') ? null : 20);
+      }
+
+      function changeOpacity() {
+        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+      }
+
+      // Heatmap data: 500 Points
+      function getPoints() {
+        return ['''
+        
+    with open("temp.txt","r") as fi:
+        reader = csv.reader(fi)
+        first = True
+        for row in reader:
+            if first:
+                html += "new google.maps.LatLng("+row[0]+","+row[1]+")"
+                first = False
+            else:
+                html += ",new google.maps.LatLng("+row[0]+","+row[1]+")"
+        
+    html += '''];
+      }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCprBsoVtLAhqFBrOSPfvMczXFUwl0DrAM&libraries=visualization&callback=initMap">
+    </script>
+  </body>
+</html>'''
+
+    with open(filename, "w") as f:
+        f.write(html)
